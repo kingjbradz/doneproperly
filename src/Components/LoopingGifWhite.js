9ef894cd87d53fa22c931983e25cfe02bbf12d0c
kingjbradz <jordanrhysbradley@gmail.com>
2023-04-14 21:49:39 +0200

    changing gifs

--- src/Components/LoopingGifWhite.js
+++ src/Components/LoopingGifWhite.js
@@ -3,7 +3,7 @@ import React from "react"
 const LoopingGifWhite = () => {
     return (
         <img
-        src="https://d1g9ggdtg3kjm9.cloudfront.net/done_properly_230413-w_1.gif"
+        src="https://d1g9ggdtg3kjm9.cloudfront.net/done_properly_230413-50.gif"
         alt="moving img"
         style={{ maxWidth: "100%" }}
         />
