53136ff56207070298df3615c19c8d6530c6cb80
kingjbradz <jordanrhysbradley@gmail.com>
2023-04-18 15:34:27 +0200

    updated marquee for workshop #2

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,12 +3,23 @@ import { Box, Typography } from "@mui/material";
 import Marquee from "react-fast-marquee";
 
 const Footer = () => {
+  const marginSpan = <span style={{marginRight:"40px"}}></span>
   return (
     <Box sx={{ width: "100%", bgcolor: "brand.secondary", height: "40px", paddingTop: 0.5 }}>
       <Marquee gradient={false} speed={75} pauseOnHover={true}>
         <Typography 
-            variant="h4" sx={{ color: "brand.primary", paddingLeft: 2, cursor: "pointer" }} onClick={() => window.location.href = "https://forms.gle/aBRU2rigJj767scZA" }>
-            WORKSHOP #2 COMING UP SOON! WORKSHOP #2 COMING UP SOON! WORKSHOP #2 COMING UP SOON!
+            variant="h4" sx={{ color: "brand.primary", paddingLeft: 2, cursor: "pointer" }} onClick={() => window.location.href = "https://forms.gle/Ejmfsd2Zr68om7sR8" }>
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
+            WORKSHOP #2 COMING UP SOON!{marginSpan}
         </Typography>
       </Marquee>
     </Box>
