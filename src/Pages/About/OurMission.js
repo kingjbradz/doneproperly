684fc84f73d2c7cfd9f85b63a4e1917f3a0f1da4
DONE PROPERLY BERLIN <129437049+dpberlin@users.noreply.github.com>
2023-09-18 21:13:24 +0200

    Update OurMission.js

--- src/Pages/About/OurMission.js
+++ src/Pages/About/OurMission.js
@@ -18,7 +18,7 @@ const OurMission = ({ themeMode }) => {
             }}
           >
             The music scene, done properly. We believe music belongs to everyone and everyone belongs to music. We bring opportunities, skill, and knowledge to anyone who might struggle to find resources to learn how to DJ. Our collective also gives a platform and a community to DJs across all genres, no matter who you are, and therefore creating more spaces for people within the music industry to thrive and belong.
-            <br />
+            <br /><br />
             Besides that, we believe accessibility and equal pay are necessary to make sure that the music industry is able to bloom. The point is not to change the way we experience the music scene, but to make sure it is fair and stabilised.
           </Typography>
         </RowBox>
