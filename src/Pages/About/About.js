ceabd9ef7f252e0f813c49d5ea308f0cb51d9fe5
DONE PROPERLY BERLIN <129437049+dpberlin@users.noreply.github.com>
2023-09-18 20:45:16 +0200

    Update About.js

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -41,43 +41,43 @@ const AboutUs = ({ themeMode }) => {
             </Typography>
           </RowBox>
         </LineBox>
-        // <LineBox sx={{ flexDirection: !is1000 && "column-reverse" }}>
-        //   <RowBox>
-        //     <Typography sx={{ color:"brand.secondary",fontSize:"14px", maxWidth: "730px !important" }}>
+ // <LineBox sx={{ flexDirection: !is1000 && "column-reverse" }}>
+ //          <RowBox>
+ //            <Typography sx={{ color:"brand.secondary",fontSize:"14px", maxWidth: "730px !important" }}>
 
-        //     We’re an LGBTQIA+, BIPOC and FLINTA* friendly space. We’ll always
-        //     prioritize those who are less privileged but everyone who respects
-        //     our values is welcome: DJ newbies, DJs with skills to share, graphic
-        //     designers, videographers, photographers who want to be part of a
-        //     community, etc. Shoot us a message to find out how you can get
-        //     involved  or click <Link href="https://docs.google.com/forms/d/e/1FAIpQLScTPJWT7WLkgy26xdZnbTxcE8lOrRBWziPr8LY9zrWFiRxhYg/viewform?pli=1" target="_blank"><u>here</u></Link>.
-        //     <br />
-        //     <br />
-        //     We’re trying to build a community where you belong: the loner, the
-        //     castaway, the nerd, the underdog. We’re bringing something that is
-        //     made for you. Together we make sure it’s done properly.
-        //     <br />
-        //     <br /> Come by and check out one of our events where we’d love for
-        //     you to stick around, connect with others, and become a real part of
-        //     our community.
-        //     <br />
-        //     <br />
-        //     After all, it ain‘t done properly without you.
-        //     </Typography>
-        //   </RowBox>
-        //   <RowBox>
-        //     <Typography
-        //       variant="h4"
-        //       sx={{
-        //         color: "brand.secondary",
-        //         width: is750 ? 750 : "auto",
-        //         marginBottom: !is1000 && 2,
-        //       }}
-        //     >
-        //       BUILDING A COMMUNITY
-        //     </Typography>
-        //   </RowBox>
-        // </LineBox>
+            // We’re an LGBTQIA+, BIPOC and FLINTA* friendly space. We’ll always
+            // prioritize those who are less privileged but everyone who respects
+            // our values is welcome: DJ newbies, DJs with skills to share, graphic
+            // designers, videographers, photographers who want to be part of a
+            // community, etc. Shoot us a message to find out how you can get
+            // involved  or click <Link href="https://docs.google.com/forms/d/e/1FAIpQLScTPJWT7WLkgy26xdZnbTxcE8lOrRBWziPr8LY9zrWFiRxhYg/viewform?pli=1" target="_blank"><u>here</u></Link>.
+            // <br />
+            // <br />
+            // We’re trying to build a community where you belong: the loner, the
+            // castaway, the nerd, the underdog. We’re bringing something that is
+            // made for you. Together we make sure it’s done properly.
+            // <br />
+            // <br /> Come by and check out one of our events where we’d love for
+            // you to stick around, connect with others, and become a real part of
+            // our community.
+            // <br />
+            // <br />
+            // After all, it ain‘t done properly without you.
+            // </Typography>
+          // </RowBox>
+          // <RowBox>
+          //   <Typography
+          //     variant="h4"
+          //     sx={{
+          //       color: "brand.secondary",
+          //       width: is750 ? 750 : "auto",
+          //       marginBottom: !is1000 && 2,
+          //     }}
+          //   >
+          //     BUILDING A COMMUNITY
+          //   </Typography>
+          // </RowBox>
+        // </LineBox> 
       </Stack>
     </>
   );
