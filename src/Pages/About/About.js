4e0107e05a6f7989907088fff8480193aee3cb2a
kingjbradz <jordanrhysbradley@gmail.com>
2023-12-02 13:34:16 +0100

    big round of updates

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,23 +3,25 @@ import { Typography, useMediaQuery, Stack } from "@mui/material";
 import { LineBox, RowBox } from "../../Components/Elements";
 
 const AboutUs = ({ themeMode }) => {
+  const is335 = useMediaQuery("( min-width: 335px )");
   const is750 = useMediaQuery("( min-width: 750px )");
-  const is1000 = useMediaQuery("( mind-width: 1000px )");
+  const is1000 = useMediaQuery("( min-width: 1000px )");
   return (
     <>
       <Stack
         direction="column"
         justifyContent="center"
         alignItems="center"
+        sx={{ height: is335 ? "100%" : "auto" }}
       >
-        <LineBox sx={{ flexDirection: !is1000 && "column-reverse" }}>
+        <LineBox sx={{ flexDirection: !is1000 && "column-reverse", marginBottom: 0 }}>
           <RowBox>
             <Typography
               variant="h4"
               sx={{
                 color: "brand.secondary",
                 width: is750 ? 750 : "auto",
-                marginBottom: !is1000 && 2,
+                // marginBottom: !is1000 && 2,
               }}
             >
               <b>DONE PROPERLY</b> IS A QUEER-OWNED MUSIC COLLECTIVE OPERATING IN BERLIN WITH THE OBJECTIVE OF MAKING MUSIC EASILY ACCESSIBLE TO ALL, THROUGH SKILL-SHARING, CREATING DIVERSE PLATFORMS AND EASY ACCESS TO EQUIPMENT.
--- src/Pages/AboutContainer.js
+++ src/Pages/AboutContainer.js
@@ -14,49 +14,51 @@ import WhatWeOffer from "./About/WhatWeOffer";
 
 const AboutContainer = ({ themeMode }) => {
   return (
-    <Box>
+    <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "space-between", height: "100%" }}>
       <About themeMode={themeMode} />
-      <Accordion elevation={0} disableGutters={true}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon sx={{ color: "brand.primary" }} />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          sx={{ color: "brand.primary", bgcolor: "brand.secondary", display: "flex", justifyContent: "center", width: "100%" }}
-        >
-          <Box sx={{  }}>
-          <Typography>OUR MISSION</Typography>
-          </Box>
-        </AccordionSummary>
-        <AccordionDetails>
-         <OurMission themeMode={themeMode} />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion elevation={0} disableGutters={true}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon sx={{ color: "brand.primary" }} />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          sx={{ color: "brand.primary", bgcolor: "brand.secondary" }}
-        >
-          <Typography>WHAT WE OFFER</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-         <WhatWeOffer themeMode={themeMode} />
-        </AccordionDetails>
-      </Accordion>
-      <Accordion elevation={0} disableGutters={true}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon sx={{ color: "brand.primary" }} />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-          sx={{ color: "brand.primary", bgcolor: "brand.secondary" }}
-        >
-          <Typography>THE TEAM</Typography>
-        </AccordionSummary>
-        <AccordionDetails>      
-          <Team themeMode={themeMode} />
-        </AccordionDetails>
-      </Accordion>
+      <Box>
+        <Accordion elevation={0} disableGutters={true}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: "brand.primary" }} />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            sx={{ color: "brand.primary", bgcolor: "brand.secondary", display: "flex", justifyContent: "center", width: "100%" }}
+          >
+            <Box sx={{  }}>
+            <Typography>OUR MISSION</Typography>
+            </Box>
+          </AccordionSummary>
+          <AccordionDetails>
+          <OurMission themeMode={themeMode} />
+          </AccordionDetails>
+        </Accordion>
+        <Accordion elevation={0} disableGutters={true}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: "brand.primary" }} />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            sx={{ color: "brand.primary", bgcolor: "brand.secondary" }}
+          >
+            <Typography>WHAT WE OFFER</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+          <WhatWeOffer themeMode={themeMode} />
+          </AccordionDetails>
+        </Accordion>
+        <Accordion elevation={0} disableGutters={true}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon sx={{ color: "brand.primary" }} />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            sx={{ color: "brand.primary", bgcolor: "brand.secondary" }}
+          >
+            <Typography>THE TEAM</Typography>
+          </AccordionSummary>
+          <AccordionDetails>      
+            <Team themeMode={themeMode} />
+          </AccordionDetails>
+        </Accordion>
+      </Box>
     </Box>
   );
 };
